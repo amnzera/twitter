0435eb0ee2dc9900afa97ab85e021a4cefa0ca6a
amn_s <Amnzera160@>
2023-01-02 16:11:10 -0300

    ajustes commit

--- src/app/shared/components/card-line/card-line.component.ts
+++ src/app/shared/components/card-line/card-line.component.ts
@@ -1,6 +1,5 @@
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {Timeline} from "@data/interface/timeline";
-import {BsModalRef} from "ngx-bootstrap/modal";
 import {ModalRemoveComponent} from "../modal-remove/modal-remove.component";
 
 @Component({
@@ -26,15 +25,15 @@ export class CardLineComponent implements OnInit, OnDestroy {
   loopforTime(){
     this.clockHandle = setInterval(()=> {
       // @ts-ignore
-      var inicio = new Date(this.timeItem?.date);
-      var fim = new Date();
-      var diferenca = (new Date(fim.valueOf() - inicio.valueOf())).toISOString();
+      var begin = new Date(this.timeItem?.date);
+      var current = new Date();
+      var diference = (new Date(current.valueOf() - begin.valueOf())).toISOString();
 
 
-      if(Date.parse(diferenca) < 60000) {
+      if(Date.parse(diference) < 60000) {
         this.dateNow = 'Agora';
       } else {
-        this.dateNow = new Date().toLocaleString();
+        this.dateNow = new Date().toLocaleString().slice(0, -3);
       }
 
     },1000);
